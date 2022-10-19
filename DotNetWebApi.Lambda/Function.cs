bbb6f9ec2707f5e0dba4414ff0955c0ea2dba7e9
Sowndarrajan Jayapal <codeninja.sj@gmail.com>
2022-10-20 00:38:50 +0100

    Added minimum required code to convert the lambda function into REST API

--- DotNetWebApi.Lambda/Function.cs
+++ DotNetWebApi.Lambda/Function.cs
@@ -1,21 +1,15 @@
 using Amazon.Lambda.Core;
+using Microsoft.AspNetCore.Hosting;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace DotNetWebApi.Lambda;
 
-public class Function
-{
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+public class Function : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+{ 
+    protected override void Init(IWebHostBuilder builder)
     {
-        return input.ToUpper();
+        builder.UseStartup<Startup>();
     }
 }
